9696150aed1a6fee085dbd82af05eb343e0a5417
Col Harris <col.w.harris@gmail.com>
2016-07-20 00:25:48 +0700

    First attempt at getting AWSIoT to build without iOS

---
+++ AWSIoTMac/AWSIoTMac.h
@@ -0,0 +1,19 @@
+//
+//  AWSIoTMac.h
+//  AWSIoTMac
+//
+//  Created by Col Harris on 19/07/2016.
+//  Copyright © 2016 Amazon Web Services. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AWSIoTMac.
+FOUNDATION_EXPORT double AWSIoTMacVersionNumber;
+
+//! Project version string for AWSIoTMac.
+FOUNDATION_EXPORT const unsigned char AWSIoTMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWSIoTMac/PublicHeader.h>
+
+
